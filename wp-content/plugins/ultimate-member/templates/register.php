02c44e77ef5e190b2b9ccd64b6f6f57038232244
jerry <jerry@jerrydeMacBook-Pro.local>
2017-06-15 00:16:57 +0800

    手机验证

--- wp-content/plugins/ultimate-member/templates/register.php
+++ wp-content/plugins/ultimate-member/templates/register.php
@@ -1,27 +1,57 @@
 <div class="um <?php echo $this->get_class( $mode ); ?> um-<?php echo $form_id; ?>">
 
-	<div class="um-form">
-	
-		<form method="post" action="">
-	
-		<?php
+    <div class="um-form">
 
-			do_action("um_before_form", $args);
-			
-			do_action("um_before_{$mode}_fields", $args);
-			
-			do_action("um_main_{$mode}_fields", $args);
-			
-			do_action("um_after_form_fields", $args);
-			
-			do_action("um_after_{$mode}_fields", $args);
-			
-			do_action("um_after_form", $args);
-			
-		?>
-		
-		</form>
-	
-	</div>
-	
-</div>
+        <form method="post" action="">
+
+            <?php
+
+            do_action("um_before_form", $args);
+
+            do_action("um_before_{$mode}_fields", $args);
+
+            do_action("um_main_{$mode}_fields", $args);
+
+            do_action("um_after_form_fields", $args);?>
+
+            <div class="um-field um-field-address um-field-text" data-key="address">
+                <div class="um-field-label">
+                    <label for="mobile-phone">手机号码</label>
+                    <span class="um-tip um-tip-w" title="手机号码">
+                        <i class="um-icon-help-circled"></i></span>
+                    <div class="um-clear"></div>
+                </div>
+                <div class="um-field-area">
+                    <input autocomplete="off" class="um-form-field valid " type="text" name="mobile-phone" id="mobile-phone" value="" placeholder="" data-validate="" data-key="address">
+                    <input id="sms_verify_button" value="获取验证码" type="button" onclick=alert("配置服务运营商"); />
+                </div>
+            </div>
+
+            <div class="um-field um-field-address um-field-text" data-key="address">
+                <div class="um-field-label">
+                    <label for="sms-code">短信验证码</label>
+                    <span class="um-tip um-tip-w" title="短信验证码">
+                        <i class="um-icon-help-circled"></i></span>
+                    <div class="um-clear"></div>
+                </div>
+                <div class="um-field-area">
+                    <input autocomplete="off" class="um-form-field valid " type="text" name="sms-code" id="sms-code" value="" placeholder="" data-validate="" data-key="address">
+                </div>
+            </div>
+
+            <!--<label for="sms_verify">手机号：</label> <input id="sms_verify" type="text"> <input id="sms_verify_button" value="获取验证码" type="button" onclick=alert("是否可以在此插入短信验证"); />
+
+            <div>
+                <label for="sms_verify">验证码：</label> <input id="sms_verify" type="text">
+
+            </div>
+-->
+            <?php do_action("um_after_{$mode}_fields", $args);
+
+            do_action("um_after_form", $args);
+
+            ?>
+
+        </form>
+
+    </div>
