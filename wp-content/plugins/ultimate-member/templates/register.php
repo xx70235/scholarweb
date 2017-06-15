519ab5201d66e8116d7c959ffd0e4970b1c85d67
xx70235 <erkofs@gmail.com>
2017-06-15 19:37:45 +0800

    测试修改注册页面

--- wp-content/plugins/ultimate-member/templates/register.php
+++ wp-content/plugins/ultimate-member/templates/register.php
@@ -7,15 +7,16 @@
 		<?php
 
 			do_action("um_before_form", $args);
-			
-			do_action("um_before_{$mode}_fields", $args);
-			
+
+//			do_action("um_before_{$mode}_fields", $args);
+
 			do_action("um_main_{$mode}_fields", $args);
-			
-			do_action("um_after_form_fields", $args);
-			
-			do_action("um_after_{$mode}_fields", $args);
-			
+
+			do_action("um_after_form_fields", $args);?>
+            <label for="sms_verify">短信验证：</label> <input id="sms_verify" type="text"> <input id="sms_verify_button" value="获取验证码" type="button" onclick=alert("是否可以在此插入短信验证"); />
+
+		<?php do_action("um_after_{$mode}_fields", $args);
+
 			do_action("um_after_form", $args);
 			
 		?>
@@ -24,4 +25,3 @@
 	
 	</div>
 	
-</div>
