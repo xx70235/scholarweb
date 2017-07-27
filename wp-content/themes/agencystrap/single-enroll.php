318edaa944bca01c5ab6374c0013aab40af7a2c8
root <root@iZj6cfth7vpqjc0chqt9i4Z.(none)>
2017-07-27 14:21:54 +0800

    up

--- wp-content/themes/agencystrap/single-enroll.php
+++ wp-content/themes/agencystrap/single-enroll.php
@@ -11,6 +11,7 @@ get_header(); ?>
 		<main id="main" class="site-main" itemprop="mainContentOfPage" itemscope="itemscope" itemtype="http://schema.org/Blog">
 
 		<?php while ( have_posts() ) : the_post(); ?>
+<?php echo "<input id='post-id' value='".$post->ID."' style='display:none;'/>";?>
 
 			<?php get_template_part( 'template-parts/content', 'single-enroll' ); ?>
 
@@ -27,4 +28,26 @@ get_header(); ?>
 	</div><!-- #primary -->
 <?php get_sidebar('2'); ?>
 </div>
+<script type="text/javascript">
+$(function(){ 
+	$(document.body).on('click','.social-share-icon',function(){
+		var id = $("#post-id").val();
+		$.post("/count.php",
+          {
+              pid:id,
+          },
+          function(data,status){
+              if ('success'==status)
+              {
+                  
+              }
+              else
+              {
+                  
+              }
+          });
+		
+	})
+});
+</script>
 <?php get_footer(); ?>
