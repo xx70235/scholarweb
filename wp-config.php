3e3407832998449b2c3fbd5b2fbfff8fdc63dbde
Xu Yunfei <xuyf@nao.cas.cn>
2017-08-08 23:10:30 +0800

    更新搜索功能

--- wp-config.php
+++ wp-config.php
@@ -79,11 +79,14 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 define('WPLANG', 'zh_CN');
+define('FS_METHOD','direct');
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
+
+
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
