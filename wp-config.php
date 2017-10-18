0806ca9fa5d138b09f0c66359c3142fb1fc5560a
xx70235 <erkofs@gmail.com>
2017-10-18 22:37:44 +0800

    config update

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'scholar');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '!Cas15801588196');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
